d57e5f487f2f9a737f05462a8e655889471918e9
zero <zero.griffin@gmail.com>
2018-06-20 10:34:23 +0200

    Update colours

--- htdocs/index.php
+++ htdocs/index.php
@@ -120,7 +120,7 @@
     <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
   </div>
 
-  <footer class="page-footer teal">
+  <footer class="page-footer darken">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
