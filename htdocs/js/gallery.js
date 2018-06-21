013f519442aaa82cad8de781ab572a2ce2147814
zero <zero.griffin@gmail.com>
2018-06-21 15:46:17 +0200

    Add carousel

---
+++ htdocs/js/gallery.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
